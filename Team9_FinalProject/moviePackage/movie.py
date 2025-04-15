9f0cb0cee269653d8e4ee1a37e52cd093e1b5407
carfornc <carfornc@LCB-10>
2025-04-15 13:50:51 -0400

    Added documentation, started location decryption but it is not done it needs edits

--- Team9_FinalProject/moviePackage/movie.py
+++ Team9_FinalProject/moviePackage/movie.py
@@ -1,1 +1,13 @@
+# File Name : movie.py
+# Student Name: Nikki Carfora, David Becker, Richie James, Michael Slivinski
+# email:   carfornc@mail.uc.edu, beckerd8@mail.uc.edu, slivinmb@mail.uc.edu, james2c4@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date:   4/30/25
+# Course #/Section:   IS4010 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Complete a series of tasks with .json encyrpted files to complete a data scavenger hunt. 
 
+# Brief Description of what this module does.
+# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
+
+# Anything else that's relevant:
